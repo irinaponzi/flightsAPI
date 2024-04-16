9649a8751289e5776c5d91c0ab7a020085f77a20
irinaponzi <irina.ponzi@gmail.com>
2024-04-16 17:09:31 -0300

    se agregan los tests unitarios del Utils

--- src/test/java/com/codeki/flightsapi/utils/UtilsTest.java
+++ src/test/java/com/codeki/flightsapi/utils/UtilsTest.java
@@ -1,6 +1,8 @@
 package com.codeki.flightsapi.utils;
 
 import com.codeki.flightsapi.dto.FlightDto;
+import com.codeki.flightsapi.model.Company;
+import com.codeki.flightsapi.model.Dollar;
 import com.codeki.flightsapi.model.Flight;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
@@ -16,16 +18,18 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
-public class FlightUtilsTest {
+public class UtilsTest {
 
     @Autowired
-    FlightUtils flightUtils;
+    Utils utils;
 
     static List<Flight> flightList;
     static List<FlightDto> flightDtoList;
+    static Company company;
     static LocalDateTime departureTime;
     static LocalDateTime arrivingTime;
-    static Double dollar;
+    static Dollar dollar;
+    static Double dollarPrice;
 
     @BeforeAll
     static void setUp() {
@@ -34,47 +38,48 @@ public class FlightUtilsTest {
         flightDtoList = new ArrayList<>();
         departureTime = LocalDateTime.of(2024, 3, 1, 10, 0, 0);
         arrivingTime = LocalDateTime.of(2024, 3, 1, 12, 0, 0);
-        dollar = 1200.0;
+        company = new Company(1L, "Aerolíneas Argentinas", "https://www.aerolineas.com.ar/", "banner");
+        dollarPrice = 1200.0;
 
-        flightList.add(new Flight(1L, "EZE", "COR", departureTime, arrivingTime, 10.0, "Semanal"));
-        flightList.add(new Flight(2L, "EZE", "USU", departureTime, arrivingTime, 80.0, "Mensual"));
-        flightList.add(new Flight(3L, "EZE", "MDQ", departureTime, arrivingTime, 8.0, "Diario"));
+        flightList.add(new Flight(1L, "EZE", "COR", departureTime, arrivingTime, 10.0, "Semanal", company));
+        flightList.add(new Flight(2L, "EZE", "USU", departureTime, arrivingTime, 80.0, "Mensual", company));
+        flightList.add(new Flight(3L, "EZE", "MDQ", departureTime, arrivingTime, 8.0, "Diario", company));
 
-        flightDtoList.add(new FlightDto(1L, "EZE", "COR", departureTime, arrivingTime, 10.0 * dollar, "Semanal"));
-        flightDtoList.add(new FlightDto(2L, "EZE", "USU", departureTime, arrivingTime, 80.0 * dollar, "Mensual"));
-        flightDtoList.add(new FlightDto(3L, "EZE", "MDQ", departureTime, arrivingTime, 8.0 * dollar, "Diario"));
+        flightDtoList.add(new FlightDto(1L, "EZE", "COR", departureTime, arrivingTime, 10.0 * dollarPrice, "Semanal", company));
+        flightDtoList.add(new FlightDto(2L, "EZE", "USU", departureTime, arrivingTime, 80.0 * dollarPrice, "Mensual", company));
+        flightDtoList.add(new FlightDto(3L, "EZE", "MDQ", departureTime, arrivingTime, 8.0 * dollarPrice, "Diario", company));
     }
 
-    @Test
-    void detectOffersTest() {
-        Integer offerPrice = 10;
-        List<FlightDto> offersExpected = Arrays.asList(flightDtoList.get(0), flightDtoList.get(2));
-
-        List<FlightDto> offersActual = flightUtils.detectOffers(flightList, offerPrice);
-
-        assertEquals(offersExpected.size(), offersActual.size());
-        assertEquals(offersExpected.get(0).getId(), offersActual.get(0).getId());
-        assertEquals(offersExpected.get(1).getId(), offersActual.get(1).getId());
-    }
-
-    @Test
+   @Test
     void flightMapperTest() {
         Flight flight = flightList.get(0);
         FlightDto flightDtoExpected = flightDtoList.get(0);
 
-        FlightDto flightDtoActual = flightUtils.flightMapper(flight, dollar);
+       FlightDto flightDtoActual = utils.flightMapper(flight, dollarPrice);
 
-        assertEquals(flightDtoExpected.getConvertedPrice(), flightDtoActual.getConvertedPrice());
-        assertEquals(flightDtoExpected.getId(), flightDtoActual.getId());
+       assertEquals(flightDtoExpected.getConvertedPrice(), flightDtoActual.getConvertedPrice());
+       assertEquals(flightDtoExpected.getId(), flightDtoActual.getId());
     }
 
     @Test
     void flightsListMapperTest() {
         List<FlightDto> flightDtoListExpected = flightDtoList;
-        List<FlightDto> flightDtoListActual = flightUtils.flightsListMapper(flightList, dollar);
+        List<FlightDto> flightDtoListActual = utils.flightsListMapper(flightList, dollarPrice);
 
         assertEquals(flightDtoListExpected.size(), flightDtoListActual.size());
         assertThat(flightDtoListActual)
                 .allMatch(f -> f instanceof FlightDto);
+    }
+
+    @Test
+    void detectOffersTest() {
+        Double offerPrice = 10.0;
+        List<Flight> offersExpected = Arrays.asList(flightList.get(2), flightList.get(0));
+
+        List<Flight> offersActual = utils.detectOffers(flightList, offerPrice);
+
+        assertEquals(offersExpected.size(), offersActual.size());
+        assertEquals(offersExpected.get(0).getId(), offersActual.get(0).getId());
+        assertEquals(offersExpected.get(1).getId(), offersActual.get(1).getId());
     }
 }
