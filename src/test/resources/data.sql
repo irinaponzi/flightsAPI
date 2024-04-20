fdca8970f291c3eb9b55c17ca77698b52114bbd7
irinaponzi <irina.ponzi@gmail.com>
2024-04-20 15:07:53 -0300

    se agrega script de carga de la bd de test

--- src/test/resources/data.sql
+++ src/test/resources/data.sql
@@ -5,3 +5,13 @@ VALUES
     ('Flybondi', 'https://flybondi.com/ar', 'banner2'),
     ('Jet Smart', 'https://jetsmart.com/ar', 'banner3');
 
+INSERT INTO `flight`
+(`origin`, `destiny`, `departure_time`, `arriving_time`, `price`, `frequency`, `company_id`)
+VALUES
+    ('EZE', 'COR', '2024-02-01T20:00:00', '2024-02-01T22:00:00', 10.0, 'Diario', 1),
+    ('USU', 'EZE', '2024-02-01T20:00:00', '2024-02-01T22:00:00', 20.0, 'Semanal', 1),
+    ('EZE', 'JUJ', '2024-02-01T20:00:00', '2024-02-01T22:00:00', 15.0, 'Diario', 1);
+
+
+
+
